931460db59dd7c2ed65c949a86545cf0c2cec03c
llinke1 <llinke@astro.uni-bonn.de>
2021-02-15 14:29:03 +0100

    Reading in from triangles now uses c++ vector

--- classed_version/helper.hpp
+++ classed_version/helper.hpp
@@ -4,36 +4,37 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 struct treecorr_bin
 {
-	double r,u,v;
+  double r,u,v;
 };
 
-int read_triangle_configurations(std::string& infile, treecorr_bin* triangle_config, int rbins, int ubins, int vbins)
+int read_triangle_configurations(std::string& infile, std::vector<treecorr_bin>& triangle_config)
 {
-  //This should include a check that the array triangle_config is actually large enough! Or use a c++ vector, into which the values are pushed
-	std::ifstream fin(infile);
-	std::cout << "Reading " << infile << "\n";
-	if(!fin.is_open()) //checking if file can be opened
-	{
-		std::cerr << "Could not open input file. Exiting. \n";
-		exit(1);
-	}
-	while(!fin.eof())
-	{
-		int rind,uind,vind;
-		double r,u,v;
-		fin >> rind >> uind >> vind >> r >> u >> v;
-		int idx = rind*ubins*vbins + uind*vbins + vind-vbins;
-		triangle_config[idx].r = r;
-		triangle_config[idx].u = u;
-		triangle_config[idx].v = v;
-	}
+  std::ifstream fin(infile);
+  std::cout << "Reading " << infile << "\n";
+  if(!fin.is_open()) //checking if file can be opened
+    {
+      std::cerr << "Could not open input file. Exiting. \n";
+      exit(1);
+    }
+  while(!fin.eof())
+    {
+      int rind,uind,vind;
+      double r,u,v;
+      fin >> rind >> uind >> vind >> r >> u >> v;
+      treecorr_bin bin;
+      bin.r=r;
+      bin.u=u;
+      bin.v=v;
+      triangle_config.push_back(bin);
+    }
 
-	fin.close();
+  fin.close();
 
-	return 1;
+  return 1;
 
 }
 
