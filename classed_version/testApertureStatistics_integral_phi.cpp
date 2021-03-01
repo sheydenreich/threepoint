a764e5bf8d630827861151f1e9f9afceb880a859
llinke1 <llinke@astro.uni-bonn.de>
2021-03-01 18:03:50 +0100

    Executable to test phi-integral

---
+++ classed_version/testApertureStatistics_integral_phi.cpp
@@ -0,0 +1,89 @@
+#include "apertureStatistics.hpp"
+#include <fstream>
+
+/**
+ * @file testApertureStatistics_phi.cpp
+ * This executable gives the phi Integral of Eq. 58 in Schneider, Kilbinger & Lomabrdi (2003)
+ * used for debugging
+ * @author Laila Linke
+ * @warning output is written in file "../tests/TestIntegralPhi.dat"
+ * @todo Outputfilename should be read from command line
+ */
+
+int main()
+{
+  //Set up cosmology
+  struct cosmology cosmo;
+
+  if(slics)
+    {
+      printf("using SLICS cosmology...");
+      cosmo.h=0.6898;     // Hubble parameter
+      cosmo.sigma8=0.826; // sigma 8
+      cosmo.omb=0.0473;   // Omega baryon
+      cosmo.omc=0.2905-cosmo.omb;   // Omega CDM
+      cosmo.ns=0.969;    // spectral index of linear P(k)
+      cosmo.w=-1.0;
+      cosmo.om = cosmo.omb+cosmo.omc;
+      cosmo.ow = 1-cosmo.om;
+    }
+  else
+    {
+      printf("using Millennium cosmology...");
+      cosmo.h = 0.73;
+      cosmo.sigma8 = 0.9;
+      cosmo.omb = 0.045;
+      cosmo.omc = 0.25 - cosmo.omb;
+      cosmo.ns = 1.;
+      cosmo.w = -1.0;
+      cosmo.om = cosmo.omc+cosmo.omb;
+      cosmo.ow = 1.-cosmo.om;
+    }
+
+  if(test_analytical)
+    {
+      std::cout<<"Warning: Doing analytical test"<<std::endl;
+    };
+
+  //Initialize Bispectrum
+  int n_z=200; //Number of redshift bins for grids
+  double z_max=2; //maximal redshift
+  bool fastCalc=false; //whether calculations should be sped up
+  BispectrumCalculator bispectrum(cosmo, n_z, z_max, fastCalc);
+
+  //Initialize Aperture Statistics
+  ApertureStatistics apertureStatistics(&bispectrum);
+
+  //Ls for which integral is calculated
+  std::vector<double> ls{1.00000000e-03, 5.99484250e-03, 3.59381366e-02,
+			 2.15443469e-01, 1.29154967e+00, 7.74263683e+00,
+			 4.64158883e+01, 2.78255940e+02, 1.66810054e+03,
+			 1.00000000e+04};
+
+  //Set up output
+  std::ofstream out;
+  out.open("../tests/TestIntegralPhi.dat");
+
+
+  // Set up thetas
+  double theta=10./60./180.*2*3.1416; //10 arcmin in rad
+  apertureStatistics.theta1_=theta;
+  apertureStatistics.theta2_=theta;
+  apertureStatistics.theta3_=theta;
+
+  // Calculate integral and give the result
+  for(unsigned int i=0; i<ls.size(); i++)
+    {
+      double l1=ls.at(i);
+      for(unsigned int j=0; j<ls.size(); j++)
+	{
+	  double l2=ls.at(j);
+	  out<<l1<<" "
+	     <<l2<<" "
+	     <<apertureStatistics.integral_phi(l1, l2)<<std::endl;
+
+	};
+    };
+
+  return 0;
+}
