e1ad2c9240a60b79551e493724d93369ecd08855
Sven Heydenreich <s6svheyd@uni-bonn.de>
2021-06-29 14:46:07 +0200

    added computation of varying cosmologies

---
+++ cuda_version/calculateGamma_varyingCosmos.cu
@@ -0,0 +1,173 @@
+#include "gamma.cuh"
+#include "bispectrum.cuh"
+#include "cuda_helpers.cuh"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <chrono> //For time measurements
+/**
+ * @file calculateGamma_varyingCosmos.cu
+ * This executable calculates Gamma^i for variations of
+ * the cosmological parameters \f$h$\f, \f$\sigma_8$\f, \f$\Omega_b$\f, 
+ * \f$n_s$\f, \f$w$\f, \f$\Omega_m$\f, and \f$\Omega_\Lambda$\f 
+ * for predefined thetas from the Takahashi+ Bispectrum
+ * Code uses CUDA and cubature library  (See https://github.com/stevengj/cubature for documentation)
+ * @author Sven Heydenreich
+ * @warning Currently only equilateral triangles
+ * @warning thetas currently hardcoded
+ * @warning Main cosmology is hardcoded (Set to either MS)
+ * @todo Thetas should be read from command line
+ * @todo cosmology should be read from init-file
+ */
+
+int main()
+{
+    
+// Set Up Cosmology
+  struct cosmology cosmo;
+  std::string outfn;
+
+  printf("using Millennium cosmology... \n");
+  cosmo.h = 0.73;
+  cosmo.sigma8 = 0.9;
+  cosmo.omb = 0.045;
+  cosmo.omc = 0.25 - cosmo.omb;
+  cosmo.ns = 1.;
+  cosmo.w = -1.0;
+  cosmo.om = cosmo.omc+cosmo.omb;
+  cosmo.ow = 1.-cosmo.om;
+  outfn="Gammas_varyingCosmos_5_perc.dat";
+
+  std::ofstream out;
+  out.open(outfn.c_str());
+  if(!out.is_open())
+    {
+      std::cerr<<"Couldn't open "<<outfn<<std::endl;
+      exit(1);
+    };
+
+  
+  double z_max=1.1; //maximal redshift
+  double dz = z_max/((double) n_redshift_bins); //redshift binsize
+  CUDA_SAFE_CALL(cudaMemcpyToSymbol(dev_A96,&A96,48*sizeof(double)));
+  CUDA_SAFE_CALL(cudaMemcpyToSymbol(dev_W96,&W96,48*sizeof(double)));
+
+
+    
+  // Set up thetas for which ApertureStatistics are calculated
+  std::vector<double> meanr{0.5,2,16};
+  std::vector<double> meanu{0.05,0.5,0.95};
+  std::vector<double> meanv{0.05,0.5,0.95};
+
+  int N_r = meanr.size();
+  int N_u = meanu.size();
+  int N_v = meanv.size();
+
+    // Set up cosmologies at which the 3pcf are
+  // This can probably be done smarter
+  // Sets each parameter to N_cosmo values between fac_min*Main Value and fac_max*Main Value
+  int N_cosmo=10; //Number of variations for each parameter
+  double fac_min=0.95; //Minimum proportion of main value for each parameter
+  double fac_max=1.05; //Maximum proportion of main value for each parameter
+  double fac_bin=(fac_max-fac_min)/N_cosmo;
+  
+  std::vector<cosmology> cosmos(N_cosmo*7); ///<container for all cosmologies
+  for(int i=0; i<N_cosmo; i++)
+    {
+      double fac=fac_min+i*fac_bin;
+      cosmology newCosmo=cosmo;
+      newCosmo.h=cosmo.h*fac;
+      cosmos.at(i)=newCosmo;
+
+      newCosmo=cosmo;
+      newCosmo.sigma8=cosmo.sigma8*fac;
+      cosmos.at(i+N_cosmo)=newCosmo;
+
+      newCosmo=cosmo;
+      newCosmo.omb=cosmo.omb*fac;
+      newCosmo.omc=newCosmo.om-newCosmo.omb;
+      cosmos.at(i+2*N_cosmo)=newCosmo;
+
+      newCosmo=cosmo;
+      newCosmo.ns=cosmo.ns*fac;
+      cosmos.at(i+3*N_cosmo)=newCosmo;
+
+      newCosmo=cosmo;
+      newCosmo.w=cosmo.w*fac;
+      cosmos.at(i+4*N_cosmo)=newCosmo;
+      
+      newCosmo=cosmo;
+      newCosmo.om=cosmo.om*fac;
+      newCosmo.omc=newCosmo.om-newCosmo.omb;
+      cosmos.at(i+5*N_cosmo)=newCosmo;
+
+      newCosmo=cosmo;
+      newCosmo.ow=cosmo.ow*fac;
+      cosmos.at(i+6*N_cosmo)=newCosmo;
+    }
+
+  compute_weights_bessel();
+
+  for(int i=0; i<N_cosmo*7; i++)
+    {
+      std::cout<<"Doing calculations for cosmology "<<i+1<<" of "<<N_cosmo*7<<std::endl;
+      auto begin=std::chrono::high_resolution_clock::now(); //Begin time measurement
+      // Initialize Bispectrum
+      set_cosmology(cosmos[i], dz, z_max);
+     
+      
+      //Needed for monitoring
+      int Ntotal=N_r*N_u*N_v;//Total number of bins that need to be calculated
+      int step=0;
+
+      out << cosmos[i].h<<" "<<cosmos[i].sigma8<<" "<<cosmos[i].omb<<" "<<cosmos[i].ns<<" "<<cosmos[i].w<<" "<<cosmos[i].om<<" "<<cosmos[i].ow<<" ";
+
+      
+      //Calculate <MapMapMap>(theta1, theta1, theta1)
+      // Calculation only for theta1=theta2=theta3
+      for (int r_ind=0; r_ind<N_r; r_ind++)
+      {
+        for (int u_ind=0; u_ind<N_r; u_ind++)
+        {
+            for (int v_ind=0; v_ind<N_r; v_ind++)
+            {
+                double r = meanr.at(r_ind);
+                double u = meanu.at(u_ind);
+                double v = meanv.at(v_ind);
+
+                double r2 = r*M_PI/180./60.;
+                double r3 = r2*u;
+                double r1 = v*r3+r2;
+
+                std::complex<double> _gamma0 = gamma0(r1,r2,r3,z_max);
+                std::complex<double> _gamma1 = gamma1(r1,r2,r3,z_max);
+                std::complex<double> _gamma2 = gamma2(r1,r2,r3,z_max);
+                std::complex<double> _gamma3 = gamma3(r1,r2,r3,z_max);
+
+                out << r << " " << u << " " << v << " ";
+                out << real(_gamma0)  << " " << imag(_gamma0)  << " ";
+                out << real(_gamma1)  << " " << imag(_gamma1)  << " ";
+                out << real(_gamma2)  << " " << imag(_gamma2)  << " ";
+                out << real(_gamma3)  << " " << imag(_gamma3)  << " ";
+                
+                step++;
+                auto end = std::chrono::high_resolution_clock::now();
+                auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+                double progress = step*1./Ntotal;
+
+                printf("\r [%3d%%] in %.2f s. Est. remaining: %.2f s. Average: %.2f s per step.",
+                static_cast<int>(progress*100),
+                elapsed.count()*1e-9,
+                (Ntotal-step)*elapsed.count()*1e-9/step,
+                elapsed.count()*1e-9/step);
+
+            }
+        }
+      }
+      out<<std::endl;
+      std::cout<<std::endl;
+      // Stop measuring time and calculate the elapsed time
+    };
+}
