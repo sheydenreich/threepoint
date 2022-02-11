257e547231534c0040ed90589346f481a873935d
llinke1 <llinke@astro.uni-bonn.de>
2022-02-11 12:58:58 +0100

    Added cosmicShear+shapenoise GRF calculation

--- python_scripts/compute_aperture_mass_correlations_of_random_fields.py
+++ python_scripts/compute_aperture_mass_correlations_of_random_fields.py
@@ -330,7 +330,9 @@ if(__name__=='__main__'):
     elif(INPUT_FILE_POWER_SPECTRUM):
         power_spectrum = np.loadtxt(args.power_spectrum_filename)
         filename = args.power_spectrum_filename.split("/")[-1]
-        savepath = '/vol/euclid6/euclid6_ssd/sven/threepoint_with_laila/results_analytic/gaussian_random_field/input_powerspectrum/'+filename.split(".")[0]
+        savepath = '/home/laila/OneDrive/1_Work/5_Projects/02_3ptStatistics/Map3_Covariances/GaussianRandomFields_cosmicShearShapenoise/'
+        #'/vol/euclid6/euclid6_ssd/sven/threepoint_with_laila/Map3_Covariances/GaussianRandomFields_cosmicShearShapenoise/'
+#'/vol/euclid6/euclid6_ssd/sven/threepoint_with_laila/results_analytic/gaussian_random_field/input_powerspectrum/'+filename.split(".")[0]
         
     res_gamma,res_kappa = compute_aperture_mass_correlations_of_gaussian_random_fields(power_spectrum,n_pix,[2,4,8,16],args.realisations,n_processes=args.processes,compute_kappa=args.compute_from_kappa)
     if not os.path.exists(savepath):
--- scripts/calculateFFTRandomFields.sh
+++ scripts/calculateFFTRandomFields.sh
@@ -1,7 +1,10 @@
 #python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 15 --power_spectrum 0 --processes 8 --realisations 4096
 
-python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 10 --power_spectrum 0 --processes 8 --realisations 4096
+# python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 10 --power_spectrum 0 --processes 8 --realisations 4096
 
-python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 20 --power_spectrum 0 --processes 8 --realisations 4096
+# python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 20 --power_spectrum 0 --processes 8 --realisations 4096
 
-python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 5 --power_spectrum 0 --processes 8 --realisations 4096
+# python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 5 --power_spectrum 0 --processes 8 --realisations 4096
+
+
+python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 10 --power_spectrum -1 --power_spectrum_filename ../necessary_files/p_ell_slics_shapenoise.dat --processes 6 --realisations 928
