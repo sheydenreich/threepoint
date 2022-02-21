913798148be6d3fd492e6f1d3631544f4407ce31
llinke1 <llinke@astro.uni-bonn.de>
2022-02-21 16:26:03 +0100

    Minor script changes

--- scripts/calculateApertureStatisticsCovariance_Laila.sh
+++ scripts/calculateApertureStatisticsCovariance_Laila.sh
@@ -27,7 +27,7 @@ mkdir -p $DIR
 # T_1^\infty
 #../cuda_version/calculateApertureStatisticsCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_10Deg_cosmicShear.dat 1 0 0 infinite
 #../cuda_version/calculateApertureStatisticsCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_15Deg_cosmicShear.dat 1 0 0 infinite
-../cuda_version/calculateApertureStatisticsCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_5Deg_cosmicShear.dat 1 0 0 infinite
+#../cuda_version/calculateApertureStatisticsCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_5Deg_cosmicShear.dat 1 0 0 infinite
 
 # T_2
 #../cuda_version/calculateApertureStatisticsCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_5Deg_cosmicShear.dat 0 1 0 square
@@ -39,3 +39,34 @@ mkdir -p $DIR
 #../cuda_version/calculateApertureStatisticsCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_10Deg_cosmicShear.dat 1 0 0 square
 #../cuda_version/calculateApertureStatisticsCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_15Deg_cosmicShear.dat 1 0 0 square
 
+# Third: SLICS
+
+DIR=/home/laila/OneDrive/1_Work/5_Projects/02_3ptStatistics/Map3_Covariances/SLICS/
+mkdir -p $DIR
+# # T_1^\infty
+#timestamp="$(date +"%Y_%m_%d_%H_%M_%S")"
+
+#CUDA_VISIBLE_DEVICES=0 ../cuda_version/calculateApertureStatisticsCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_SLICS_sigmaTwice.dat 1 0 0 infinite &> $DIR/${timestamp}.log
+
+# # T_2
+timestamp="$(date +"%Y_%m_%d_%H_%M_%S")"
+CUDA_VISIBLE_DEVICES=0 ../cuda_version/calculateApertureStatisticsCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_SLICS_sigmaTwice.dat 0 1 0 square &> $DIR/${timestamp}.log
+
+# # T_1
+# timestamp="$(date +"%Y_%m_%d_%H_%M_%S")"
+# CUDA_VISIBLE_DEVICES=0 ../cuda_version/calculateApertureStatisticsCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_SLICS.dat 1 0 0 square &> /vol/lensgpu/ssd/llinke/${timestamp}.log
+
+# # T_4
+# timestamp="$(date +"%Y_%m_%d_%H_%M_%S")"
+# CUDA_VISIBLE_DEVICES=0 ../cuda_version/calculateApertureStatisticsCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_SLICS.dat 0 0 1 infinite &> /vol/lensgpu/ssd/llinke/${timestamp}.log
+
+#SSC
+#timestamp="$(date +"%Y_%m_%d_%H_%M_%S")"
+#CUDA_VISIBLE_DEVICES=0 ../cuda_version/calculateApertureStatisticsSSC.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_SLICS.dat &> $DIR/${timestamp}.log
+
+#timestamp="$(date +"%Y_%m_%d_%H_%M_%S")"
+#CUDA_VISIBLE_DEVICES=0 ../cuda_version/testBispecSSC.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/Our_thetas.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat $DIR ../necessary_files/Covariance_SLICS.dat &> $DIR/${timestamp}.log
+
+
+# Calculate Powerspec, Bispec and Bispec Cov for SLICS
+#../classed_version/calculateBispectrumAndCovariance.x ../necessary_files/SLICS_cosmo.dat ../necessary_files/n_z_SLICS_euclid_nz_cosmos15_i24.5cut_fu08fit_dz0.01.cat  /home/laila/OneDrive/1_Work/5_Projects/02_3ptStatistics/Map3_Covariances/SLICS/ 10 0.37 108000 deg
--- scripts/calculateFFTRandomFields.sh
+++ scripts/calculateFFTRandomFields.sh
@@ -7,4 +7,4 @@
 # python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 5 --power_spectrum 0 --processes 8 --realisations 4096
 
 
-python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 10 --power_spectrum -1 --power_spectrum_filename ../necessary_files/p_ell_slics_shapenoise.dat --processes 6 --realisations 928
+python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 10 --power_spectrum -1 --power_spectrum_filename ../necessary_files/p_ell_slics_shapenoise_0.37.dat --processes 6 --realisations 928
