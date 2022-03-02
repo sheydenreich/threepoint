292738d468692b61b45318015fb0447d2a547614
sheydenreich <s6svheyd@uni-bonn.de>
2022-03-02 16:55:17 +0100

    added lognormal random fields

--- python_scripts/utility.py
+++ python_scripts/utility.py
@@ -14,6 +14,11 @@ import matplotlib.pyplot as plt
 class MyManager(multiprocessing.managers.BaseManager):
     pass
 MyManager.register('np_zeros', np.zeros, multiprocessing.managers.ArrayProxy)
+import warnings
+import scipy.integrate as integrate
+from scipy.interpolate import interp1d
+from numpy import pi,sinh,tanh,cosh
+from scipy.special import jv,jn_zeros
 
 
 def D(npix = 4096,pixsize = 1.):
@@ -34,7 +39,8 @@ def Dhat_func(npix = 4096,pixsize = 1.):
     return a
 
 
-def create_gaussian_random_field(power_spectrum, n_pix=4096,fieldsize=4.*np.pi/180,random_seed=None):
+def create_gaussian_random_field(power_spectrum, n_pix=4096,fieldsize=4.*np.pi/180,random_seed=None,
+                                fill_value = 1e-25):
     """creates gaussian random field from given power spectrum, with mean 0 and variance sigma"""
     ell_min = 0
     two_ell_max = 2.*np.pi/fieldsize*n_pix
@@ -42,7 +48,12 @@ def create_gaussian_random_field(power_spectrum, n_pix=4096,fieldsize=4.*np.pi/1
     gen = GaussianNoiseGenerator(shape=(n_pix,n_pix),side_angle=fieldsize*u.rad)
     if random_seed is None:
         random_seed = np.random.randint(0,2**32)
-    gaussian_map = gen.fromConvPower(np.array([ell_array,power_spectrum(ell_array)]),seed=random_seed,kind="linear",bounds_error=False,fill_value=0.0)
+
+    psarray = power_spectrum(ell_array)
+    if(np.any(psarray<=0)):
+        warnings.warn("Psarray contains zeros/negatives. Substituting with {}".format(fill_value))
+        psarray[psarray<=0] = fill_value
+    gaussian_map = gen.fromConvPower(np.array([ell_array,psarray]),seed=random_seed,kind="linear",bounds_error=False,fill_value=0.0)
     #gaussian_map.visualize()
     #plt.show()
     #plt.clf()
@@ -682,12 +693,14 @@ class besselintegrator:
         return pi/R*np.sum(self.warr*function(self.psiarr/R)*self.besselarr*self.psiparr)
 
 
-bi = besselintegrator(0,0.001,6.)
-def correlation_function(x,n_pix,power_spectrum,mod=1.):
-    x = x*2*np.pi/n_pix
-    factor = (2*np.pi)**2/np.pi/n_pix**4/mod**2
-
+bi = besselintegrator(0,0.00002,5.)
+def correlation_function_from_power_spectrum(x,power_spectrum):
+    factor = 1./(2*np.pi)
+    
     def power_spectrum_ell(ell):
+#         if(np.max(ell)>10**4):
+#             print(ell)
+
         return ell*power_spectrum(ell)
     
     if(hasattr(x,'__len__')):
@@ -698,11 +711,63 @@ def correlation_function(x,n_pix,power_spectrum,mod=1.):
         return factor*result
     else:
         return factor*bi.integrate(power_spectrum_ell,x)
+
     
-def correlation_function_lognormal(x,alpha,n_pix,power_spectrum,sigma=1,mod=1):
+def power_spectrum_of_correlation_function(ell,correlation_function):
+    factor = 2*np.pi
+    def correlation_function_x(x):
+        return correlation_function(x)*x
+
+    if(hasattr(ell,'__len__')):
+        ll = len(ell)
+        result = np.zeros(ll)
+        for i,l in enumerate(ell):
+            result[i] = bi.integrate(correlation_function_x,l)
+        return factor*result
+
+    else:
+        return factor*bi.integrate(correlation_function_x,x)
+
+def correlation_function_gaussian(x,correlation_function_lognormal,alpha,sigma=1):
     c = np.exp(alpha**2/2)
     A = sigma/np.sqrt(c**2-1)
-    return A**2*(np.exp(alpha**2*correlation_function(x,n_pix,power_spectrum,mod=mod))-1)
+    return np.log(correlation_function_lognormal(x)/A**2+1)/alpha**2
+
+def lognormal_to_gaussian(vals,alpha,sigma=1):
+    c = np.exp(alpha**2/2)
+    A = sigma/np.sqrt(c**2-1)
+    return sigma**2*np.log(vals/A**2+1)/alpha**2
+
+def gaussian_to_lognormal(vals,alpha,sigma=1):
+    c = np.exp(alpha**2/2)
+    A = sigma/np.sqrt(c**2-1)
+    return A**2*(np.exp(alpha**2*vals/sigma**2)-1)
+
+    
+
+def get_gaussian_power_spectrum(power_spectrum,alpha,sigma,
+                               thetas = np.arange(0.000001,8.*np.pi/180,2.*np.pi/180/1000),
+                               ells = np.geomspace(10,10**5,1000)
+                               ):
+    corr = correlation_function_from_power_spectrum(thetas,power_spectrum)
+    corr_lognormal = lognormal_to_gaussian(corr,alpha,sigma)
+
+    corrf = interp1d(thetas,corr_lognormal,fill_value=0,bounds_error=False)
+    ps = power_spectrum_of_correlation_function(ells,corrf)
+    return interp1d(ells,ps,fill_value=0,bounds_error=False)
+
+
+def get_lognormal_power_spectrum(power_spectrum,alpha,sigma,
+                               thetas = np.geomspace(0.000001,8.*np.pi/180,10000),
+                               ells = np.geomspace(10,10**5,1000)
+                               ):
+    corr = correlation_function_from_power_spectrum(thetas,power_spectrum)
+    corr_lognormal = gaussian_to_lognormal(corr,alpha,sigma)
+
+    corrf = interp1d(thetas,corr_lognormal,fill_value=0,bounds_error=False)
+    ps = power_spectrum_of_correlation_function(ells,corrf)
+    return interp1d(ells,ps,fill_value=0,bounds_error=False)
+
 
 def create_triangle(r1,r2,r3,offset = [0,0],yscale = 1.):
     x1 = np.array([0,0])
@@ -718,37 +783,42 @@ def create_triangle(r1,r2,r3,offset = [0,0],yscale = 1.):
     result[:,1]*=yscale
     return result
 
-def kappa_3pcf_lognormal(x12,x13,x23,alpha,n_pix,power_spectrum,sigma=1,mod=1,discrete=False):
-    if(discrete):
-        if(isinstance(x12,(list, tuple, np.ndarray))):
-            x12_temp = np.zeros_like(x12)
-            x13_temp = np.zeros_like(x12)
-            x23_temp = np.zeros_like(x12)
-            for i in range(len(x12)):
-                x1,x2,x3 = np.rint(create_triangle(x12[i],x13[i],x23[i]))
-                x12_temp[i] = np.linalg.norm(x1-x2)
-                x13_temp[i] = np.linalg.norm(x1-x3)
-                x23_temp[i] = np.linalg.norm(x2-x3)
-            x12 = x12_temp
-            x13 = x13_temp
-            x23 = x23_temp
-        else:
-            x1,x2,x3 = np.rint(create_triangle(x12,x13,x23))
-            x12 = np.linalg.norm(x1-x2)
-            x13 = np.linalg.norm(x1-x3)
-            x23 = np.linalg.norm(x2-x3)
-    c = np.exp(alpha**2/2)
-    A = sigma/np.sqrt(c**2-1)
-    term1 = correlation_function_lognormal(x12,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)*correlation_function_lognormal(x13,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)*correlation_function_lognormal(x23,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)
-    term21 = correlation_function_lognormal(x12,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)*correlation_function_lognormal(x13,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)
-    term22 = correlation_function_lognormal(x12,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)*correlation_function_lognormal(x23,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)
-    term23 = correlation_function_lognormal(x13,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)*correlation_function_lognormal(x23,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)
-    return term1/A**3+(term21+term22+term23)/A
+# def kappa_3pcf_lognormal(x12,x13,x23,alpha,n_pix,power_spectrum,sigma=1,mod=1,discrete=False):
+#     if(discrete):
+#         if(isinstance(x12,(list, tuple, np.ndarray))):
+#             x12_temp = np.zeros_like(x12)
+#             x13_temp = np.zeros_like(x12)
+#             x23_temp = np.zeros_like(x12)
+#             for i in range(len(x12)):
+#                 x1,x2,x3 = np.rint(create_triangle(x12[i],x13[i],x23[i]))
+#                 x12_temp[i] = np.linalg.norm(x1-x2)
+#                 x13_temp[i] = np.linalg.norm(x1-x3)
+#                 x23_temp[i] = np.linalg.norm(x2-x3)
+#             x12 = x12_temp
+#             x13 = x13_temp
+#             x23 = x23_temp
+#         else:
+#             x1,x2,x3 = np.rint(create_triangle(x12,x13,x23))
+#             x12 = np.linalg.norm(x1-x2)
+#             x13 = np.linalg.norm(x1-x3)
+#             x23 = np.linalg.norm(x2-x3)
+#     c = np.exp(alpha**2/2)
+#     A = sigma/np.sqrt(c**2-1)
+#     term1 = correlation_function_lognormal(x12,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)*correlation_function_lognormal(x13,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)*correlation_function_lognormal(x23,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)
+#     term21 = correlation_function_lognormal(x12,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)*correlation_function_lognormal(x13,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)
+#     term22 = correlation_function_lognormal(x12,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)*correlation_function_lognormal(x23,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)
+#     term23 = correlation_function_lognormal(x13,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)*correlation_function_lognormal(x23,alpha,n_pix,power_spectrum,sigma=sigma,mod=mod)
+#     return term1/A**3+(term21+term22+term23)/A
 
-def create_lognormal_random_field(power_spectrum_of_gaussian_random_field,alpha,sigma=1.,npix=4096,fieldsize=4.*np.pi/180.):
+def create_lognormal_random_field(power_spectrum_of_gaussian_random_field,alpha,sigma=1.,npix=4096,fieldsize=10.*np.pi/180.,random_seed=None):
     """Returns a lognormal field with non-gaussianity alpha"""
+    gaussian_random_field = create_gaussian_random_field(power_spectrum_of_gaussian_random_field,n_pix=npix,fieldsize=fieldsize,random_seed=random_seed)
+
     c = np.exp(alpha**2/2)
     new_field_prefactor = sigma/(c*np.sqrt(c**2-1))
-    gaussian_random_field = create_gaussian_random_field(power_spectrum_of_gaussian_random_field,npix=npix,fieldsize=fieldsize)
-    new_field = new_field_prefactor*(np.exp(alpha*gaussian_random_field)-c)
+    new_field = new_field_prefactor*(np.exp(alpha*gaussian_random_field/sigma)-c)
+#     print(np.std(new_field),sigma,np.sqrt(integrate.quad(lambda ell: ell*power_spectrum_of_gaussian_random_field(ell),
+#                                                 10,10**5)[0]/(2*np.pi)))
+#     plt.hist(gaussian_random_field.ravel(),label="grf {}".format(alpha))
+#     plt.hist(new_field.ravel(),label="lognormal {}".format(alpha))
     return new_field
