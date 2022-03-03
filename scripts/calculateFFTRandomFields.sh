7dd5fafa244080166cfb4eb987509a89f2d15a5e
Laila Linke <llinke@astro.uni-bonn.de>
2022-03-03 15:58:18 +0100

    MISC changes

--- scripts/calculateFFTRandomFields.sh
+++ scripts/calculateFFTRandomFields.sh
@@ -9,4 +9,8 @@
 
 #python ../python_scripts/compute_aperture_mass_correlations_of_random_fields.py --npix 4096 --fieldsize 10 --power_spectrum -1 --power_spectrum_filename ../necessary_files/p_ell_slics_shapenoise_0.37.dat --processes 6 --realisations 928
 
-python ../python_scripts/compute_aperture_mass_correlations_of_lognormal_fields.py --npix 4096 --fieldsize 10 --power_spectrum_filename ../necessary_files/p_ell_slics_shapenoise_0.37.dat --alpha 0.5 --processes 128 --realisations 4096
+#python ../python_scripts/compute_aperture_mass_correlations_of_lognormal_fields.py --npix 4096 --fieldsize 10 --power_spectrum_filename ../necessary_files/p_ell_slics_shapenoise_0.37.dat --alpha 0.5 --processes 128 --realisations 4096
+
+python ../python_scripts/compute_aperture_mass_correlations_of_lognormal_fields.py --npix 4096 --fieldsize 10 --power_spectrum_filename ../necessary_files/p_ell_slics_shapenoise_0.37.dat --alpha 1 --processes 256 --realisations 4096
+
+python ../python_scripts/compute_aperture_mass_correlations_of_lognormal_fields.py --npix 4096 --fieldsize 10 --power_spectrum_filename ../necessary_files/p_ell_slics_shapenoise_0.37.dat --alpha 2 --processes 256 --realisations 4096
