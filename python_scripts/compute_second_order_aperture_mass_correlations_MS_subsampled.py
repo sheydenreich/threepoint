5d0004eaed6b5ad8b5b1aff1f64576dddf1094fc
llinke1 <llinke@astro.uni-bonn.de>
2022-07-11 09:51:41 +0200

    Scripts for measuring Map2 in MS with downsampling

---
+++ python_scripts/compute_second_order_aperture_mass_correlations_MS_subsampled.py
@@ -0,0 +1,70 @@
+from matplotlib import use
+from file_loader import get_millennium_downsampled
+from utility import aperture_mass_computer,extract_second_order_aperture_masses
+import numpy as np
+import sys
+from tqdm import tqdm
+import multiprocessing.managers
+from multiprocessing import Pool
+from astropy.io import fits
+import os
+import matplotlib.pyplot as plt
+
+Ngal_subsample=288000
+
+class MyManager(multiprocessing.managers.BaseManager):
+    pass
+MyManager.register('np_zeros', np.zeros, multiprocessing.managers.ArrayProxy)
+
+process_parallel=False
+
+startpath = '/home/laila/OneDrive/1_Work/5_Projects/02_3ptStatistics/Map3_Covariances/MS/' # '/vol/euclid6/euclid6_ssd/sven/threepoint_with_laila/Map3_Covariances/MS/'
+
+def compute_aperture_masses_of_field(los,theta_ap_array,save_map=None,use_polynomial_filter=False):
+    fieldsize = 4.*60
+    npix = 4096
+    Xs, Ys, shears1, shears2 = get_millennium_downsampled(los, Ngal_subsample)
+
+    shear=shears1+1.0j*shears2
+
+    result = extract_second_order_aperture_masses(Xs, Ys,shear, npix,theta_ap_array,fieldsize,compute_mcross=False,save_map=save_map,use_polynomial_filter=use_polynomial_filter)
+
+    return result
+
+
+def compute_aperture_masses_of_field_kernel(kwargs):
+    result, los, theta_ap_array, save_map, use_polynomial_filter, realisation = kwargs
+    map2=compute_aperture_masses_of_field(los, theta_ap_array, save_map=save_map, use_polynomial_filter=use_polynomial_filter)
+    result[:,realisation]=map2
+
+def compute_all_aperture_masses(all_los,savepath,aperture_masses = [1.17,2.34,4.69,9.37],n_processes = 64,use_polynomial_filter=False):
+    n_files = len(all_los)
+    n_thetas=len(aperture_masses)
+    if(process_parallel):
+        m = MyManager()
+        m.start()
+        results=m.np_zeros((n_thetas, n_files))
+
+        with Pool(processes=n_processes) as p:
+            args=[[results, all_los[i], aperture_masses, None, use_polynomial_filter, i] for i in range(n_files)]
+            for i in tqdm(p.imap_unordered(compute_aperture_masses_of_field_kernel, args), total=n_files):
+                pass
+        np.savetxt(savepath+'map_squared_ngal_{Ngal_subsample}',results)
+    else:
+        for los in all_los:
+            print(f"Processing {los}")
+            map2=compute_aperture_masses_of_field(los, aperture_masses, save_map=None, use_polynomial_filter=use_polynomial_filter)
+            np.savetxt(savepath+f"map_squared_{los}_ngal_{Ngal_subsample}.dat", map2)
+
+if(__name__=='__main__'):
+
+    all_los = range(64)
+    # if not 'SLICS' in dirpath:
+        # dir_end_path = dirpath.split('/')[-1]
+    savepath = startpath + 'map_squared_our_thetas'
+    print('Writing summary statistics to ',savepath)
+    if not os.path.exists(savepath):
+        os.makedirs(savepath)
+
+    compute_all_aperture_masses(all_los,savepath+'/',n_processes=10,aperture_masses = [2,4,8,16])
+
--- python_scripts/file_loader.py
+++ python_scripts/file_loader.py
@@ -14,9 +14,51 @@ def get_millennium(los):
     ms_field = ms_field.reshape(4096,4096,5)
     return ms_field
 
+
+def get_millennium_downsampled(los, ngal):
+    los_no1 = los//8
+    los_no2 = los%8
+    ms_field = np.loadtxt("/home/laila/Work_Local/MS/41_los_8_"+ str(los_no1) +"_"+ str(los_no2) +".ascii")
+    indices=np.random.choice(np.arange(len(ms_field)), ngal, replace=False)
+    Xs=ms_field[indices][:,0]
+    Ys=ms_field[indices][:,1]
+    shears1=ms_field[indices][:,2]
+    shears2=ms_field[indices][:,3]
+    return Xs, Ys, shears1, shears2
+
+def get_millennium_downsampled_shapenoise(los, ngal,shapenoise):
+    los_no1 = los//8
+    los_no2 = los%8
+    ms_field = np.loadtxt("/home/laila/Work_Local/MS/41_los_8_"+ str(los_no1) +"_"+ str(los_no2) +".ascii")
+    indices=np.random.choice(np.arange(len(ms_field)), ngal, replace=False)
+
+    sigma1=0
+    sigma2=0
+    if shapenoise > 0:
+        sigma1=np.random.normal(scale=shapenoise, size=len(indices))
+        sigma2=np.random.normal(scale=shapenoise, size=len(indices))
+    
+    #print(sigma1)
+
+    Xs=ms_field[indices][:,0]
+    Ys=ms_field[indices][:,1]
+    shears1=ms_field[indices][:,2]+sigma1
+    shears2=ms_field[indices][:,3]+sigma2
+    return Xs, Ys, shears1, shears2
+
+
 def get_gamma_millennium(los):
     data = get_millennium(los)
     return data[:,:,2] + 1.0j*data[:,:,3]
+
+def get_gamma_millennium_shapenoise(los, shapenoise):
+    data = get_millennium(los)
+    sigma1=0
+    sigma2=0
+    if shapenoise > 0:
+        sigma1=np.random.normal(scale=shapenoise, size=data[:,:,0].shape)
+        sigma2=np.random.normal(scale=shapenoise, size=data[:,:,0].shape)
+    return data[:,:,2]+sigma1 + 1.0j*(data[:,:,3]+sigma2)
 
 def get_kappa_slics(los):
     filestr = "/vol/euclid7/euclid7_2/llinke/HOWLS/convergence_maps/SLICS_LCDM/kappa_noise_GalCatalog_LOS_cone"+str(los)+".fits_s333"+str(los)+"_zmin0.0_zmax3.0_sys_3.fits_ks_nomask_shear.fits"
